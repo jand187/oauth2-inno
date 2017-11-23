061e58efeae5acbe78300f1350f6f6ff67d48e74
Jan Daniel Andersen <jand187@gmail.com>
2017-11-23 15:13:51 +0100

    WIP

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,40 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using IdentityServer4.AccessTokenValidation;
 
 namespace Api
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvcCore()
+				.AddAuthorization()
+				.AddJsonFormatters();
 
-            app.UseMvc();
-        }
-    }
+			services.AddAuthentication("bearer")
+				.AddIdentityServerAuthentication(options =>
+				{
+					options.Authority = "http:localhost:5000";
+					options.RequireHttpsMetadata = false;
+
+					options.ApiName = "api1";
+				});
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+				app.UseDeveloperExceptionPage();
+
+			app.UseAuthentication();
+			app.UseMvc();
+		}
+	}
 }
